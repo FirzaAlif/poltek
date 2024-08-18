853902a5298b72f34b22539346352ed487fd9350
FirzaAlif <mhmdfirza09@gmail.com>
2024-08-19 00:31:47 +0700

    add controller

--- routes/firza.php
+++ routes/firza.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 
 
-
+Route::middleware(['auth'])->group(function () {
+    Route::resource('mahasiswas', MahasiswaController::class);
+});
 ?>
