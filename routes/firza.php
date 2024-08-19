205fbbe60c543e28ee3f10891d78812783af6aec
FirzaAlif <mhmdfirza09@gmail.com>
2024-08-19 14:55:59 +0700

    add major

--- routes/firza.php
+++ routes/firza.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MajorController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware(['auth'])->group(function () {
     Route::resource('mahasiswas', MahasiswaController::class);
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('majors', MajorController::class);
 });
 ?>
